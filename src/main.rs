626c479fe86a80c5fae4250dd07ba9b8132f49bd
luizneto <luiznetodev@gmail.com>
2023-08-07 15:11:43 -0300

    remove configuration for window

--- src/main.rs
+++ src/main.rs
@@ -34,9 +34,6 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 title: "PUZZLE".into(),
-                present_mode: bevy::window::PresentMode::AutoVsync,
-                fit_canvas_to_parent: true,
-                prevent_default_event_handling: false,
                 ..default()
             }),
             ..default()
